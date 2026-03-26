930f0d7fb22b0cb5c146121ccaad44eb09fe78ba
Olesya Orlenko <olees.orlenko@yandex.ru>
2026-03-26 17:15:12 +0300

    Create project

---
+++ AYL/AYL/AYLApp.swift
@@ -0,0 +1,17 @@
+//
+//  AYLApp.swift
+//  AYL
+//
+//  Created by Олеся Орленко on 26.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct AYLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
