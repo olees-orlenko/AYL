e09c30d1714da01c4a466564568eb84303ee3aa4
Olesya Orlenko <olees.orlenko@yandex.ru>
2026-04-14 16:29:51 +0300

    [Feature]add photos to the gallery

--- AYL/AYL/Models/GalleryItem.swift
+++ AYL/AYL/Models/GalleryItem.swift
@@ -16,10 +16,30 @@ struct GalleryItem: Identifiable {
 extension GalleryItem {
     static let mockPhotos: [GalleryItem] = [
         GalleryItem(imageName: "photo1", title: "Конференция АЮЛ"),
-        GalleryItem(imageName: "photo2", title: "Тренинг"),
-        GalleryItem(imageName: "photo1", title: "Веревочный курс"),
-        GalleryItem(imageName: "photo2", title: "Наши участники"),
-        GalleryItem(imageName: "photo1", title: "Конференция АЮЛ"),
-        GalleryItem(imageName: "photo", title: "Тренинг")
+        GalleryItem(imageName: "photo2", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo3", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo4", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo5", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo6", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo7", title: "Веревочный курс"),
+        GalleryItem(imageName: "photo8", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo9", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo10", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo11", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo12", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo13", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo14", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo15", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo16", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo17", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo18", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo19", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo20", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo21", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo22", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo23", title: "Конференция АЮЛ"),
+        GalleryItem(imageName: "photo24", title: "Веревочный курс"),
+        GalleryItem(imageName: "photo25", title: "Веревочный курс"),
+        GalleryItem(imageName: "photo26", title: "Веревочный курс")
     ]
 }
